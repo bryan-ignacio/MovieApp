3a77f7be4908c91d8c5f5c218bd5f0db1092eb01
Bryan <bryan-ignacio@outlook.com>
2024-07-21 16:29:38 -0600

    2/3 interfaz.

--- app/src/main/java/com/example/movieapp/MovieFragment.kt
+++ app/src/main/java/com/example/movieapp/MovieFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 
 class MovieFragment : Fragment(R.layout.fragment_movie) {
-    
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
