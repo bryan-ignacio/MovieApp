d22166c6ba5cec4d08b8085db099152f4bbf1edb
Bryan <bryan-ignacio@outlook.com>
2024-07-25 01:03:02 -0600

    fetch and api data.

--- app/src/main/java/com/example/movieapp/presentation/MovieViewModel.kt
+++ app/src/main/java/com/example/movieapp/presentation/MovieViewModel.kt
@@ -8,24 +8,31 @@ import com.example.movieapp.repository.MovieRepository
 import kotlinx.coroutines.Dispatchers
 
 // capa presentacion:  el repositorio le manda la data al viewModel.
-class MovieViewModel(private val repo:MovieRepository):ViewModel() {
+class MovieViewModel(private val repo: MovieRepository) : ViewModel() {
 
     // se comunica con el repositorio.
-    fun fetchUpcomingMovies() = liveData(Dispatchers.IO){
+    fun fetchMainScreenMovies() = liveData(Dispatchers.IO) {
         // tenemos 3 tipos de estado. carga, exito y error.
         // emite el valor de carga.
         emit(Resource.Loading())
         // llamada al servidor.
         try {
-            emit(Resource.Success(repo.getUpcomingMovies()))
-        }catch (e: Exception){
+            emit(
+                Resource.Success(
+                    Triple(
+                        repo.getUpcomingMovies(),
+                        repo.getTopRatedMovies(),
+                        repo.getPopularMovies()
+                    )
+                )
+            )
+        } catch (e: Exception) {
             emit(Resource.Failure(e))
         }
-
     }
 }
 
-class MovieViewModelFactory(private val repo: MovieRepository): ViewModelProvider.Factory {
+class MovieViewModelFactory(private val repo: MovieRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return modelClass.getConstructor(MovieRepository::class.java).newInstance(repo)
     }
--- app/src/main/java/com/example/movieapp/ui/movie/MovieFragment.kt
+++ app/src/main/java/com/example/movieapp/ui/movie/MovieFragment.kt
@@ -1,22 +1,58 @@
 package com.example.movieapp.ui.movie
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.example.movieapp.R
+import com.example.movieapp.core.Resource
+import com.example.movieapp.data.remote.MovieDataSource
 import com.example.movieapp.databinding.FragmentMovieBinding
+import com.example.movieapp.presentation.MovieViewModel
+import com.example.movieapp.presentation.MovieViewModelFactory
+import com.example.movieapp.repository.MovieRepositoryImplement
+import com.example.movieapp.repository.RetrofitClient
 
 class MovieFragment : Fragment(R.layout.fragment_movie) {
 
-// inicializamos binding.
+    // inicializamos binding.
     private lateinit var binding: FragmentMovieBinding
+
+
+    private val viewModel by viewModels<MovieViewModel> {
+        MovieViewModelFactory(
+            MovieRepositoryImplement(
+                MovieDataSource(
+                    RetrofitClient.webservice
+                )
+            )
+        )
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         // mandamos la vista al binding.
         binding = FragmentMovieBinding.bind(view)
         //acceder a los elementos que se encuentran en el layout vistas.
+        viewModel.fetchMainScreenMovies().observe(viewLifecycleOwner, Observer { result ->
+            when (result) {
+                is Resource.Loading -> {
+                    Log.d("LiveData", "Loading...")
+                }
 
+                is Resource.Success -> {
+                    Log.d(
+                        "LiveData",
+                        "Upcoming: ${result.data.first}\nTopRated:${result.data.second}\nPopular: ${result.data.third}"
+                    )
+                }
+
+                is Resource.Failure -> {
+                    Log.d("LiveData", "${result.exception}")
+                }
+            }
+        })
     }
-
 }
